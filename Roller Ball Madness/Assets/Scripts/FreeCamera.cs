0e83a8e340dbc58aac33e8215c14179b84ed2631
BryanFour <BryanFoureur@hotmail.com>
2019-04-22 20:24:25 +0800

    Camera Clipping implemented.

--- Roller Ball Madness/Assets/Scripts/FreeCamera.cs
+++ Roller Ball Madness/Assets/Scripts/FreeCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 // Video 1 - https://www.youtube.com/watch?v=A4Hyh4UdslQ&list=PLLH3mUGkfFCWCsGUfwLMnDWdkpQuqW3xa&index=12 --- Comments have alternate solutions to try
 // Video 2 - https://www.youtube.com/watch?v=Ta7v27yySKs - Restrict cameras up/down to 90 degrees
 // Video 3 - https://www.youtube.com/watch?v=KcKo8QHOjlk&list=PLLH3mUGkfFCWCsGUfwLMnDWdkpQuqW3xa&index=30 -- Buffer time(Time till game starts after selecting a level)
+// UnityDocs 1 - https://docs.unity3d.com/Manual/Layers.html -- Layer mask and bitshift info.
+// UnityDocs 2 - https://docs.unity3d.com/ScriptReference/Physics.Raycast.html -- Im using Physics.Raycast but the Physics.Linecast example found in the Layers docs helped alot. 
 
 public class FreeCamera : MonoBehaviour
 {
@@ -12,13 +14,16 @@ public class FreeCamera : MonoBehaviour
 	private const float Y_ANGLE_MIN = 8f;
 	private const float Y_ANGLE_MAX = 25f;
 
-	public Transform lookAt; // what the camera looks at
+	public Transform thePlayer; // what the camera looks at
 	public Transform camTransform; 
 
 	private Camera cam;
 
 	[Range(0, 40)][Header("Distance Value")][Tooltip("The distance between the player and the camera.")]
-	[SerializeField] private float distance = 20f; // distance between player and camera
+	[SerializeField] private float idealCamOffset = 20f; // distance between player and camera
+	private float camOffset;
+	private float camOffsetOffset = -1;
+
 
 	[Range(0, 5)][Header("Camera Sensitivity")][Tooltip("The camrea joysticks sensitivity.")]
 	[SerializeField] private float sensitivityX = 3f;
@@ -31,6 +36,11 @@ public class FreeCamera : MonoBehaviour
 	private float startTime = 0f;
 	private const float TIME_BEFORE_START = 2.5f;
 
+	// Raycast
+	public GameObject castFrom;
+	public GameObject castTo;
+
+
 	private void Start()
 	{
 		camTransform = transform;
@@ -39,30 +49,51 @@ public class FreeCamera : MonoBehaviour
 
 	private void Update()
 	{
+		//	If we havent started playing yet(If the game timer hasnt started yet)
 		if (Time.time - startTime < TIME_BEFORE_START)
 		{
-			return;
+			return;	// Dont allow the player to move the camera.
 		}
 
 		currentX += cameraJoystick.InputDirection.x * sensitivityX;
 		currentY += cameraJoystick.InputDirection.z * sensitivityY;
 
+		//	Clamp the cameras Y-axis value to a min and max, so the player cant move the camera to00 high or low.
 		currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
 	}
 
 	private void LateUpdate()
 	{
+		//	If we havent started playing yet(If the game timer hasnt started yet)
 		if (Time.time - startTime < TIME_BEFORE_START)
 		{
-			return;
+			return;	// Dont allow the player to move the camera.
 		}
 
-		Vector3 dir = new Vector3(0, 0, -distance);
-		Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-		camTransform.position = lookAt.position + rotation * dir;
-		camTransform.LookAt(lookAt.position);
-		//transform.position = lookAt.position + rotation * dir; // Commented out due to changes in Video 2
-		//transform.LookAt(lookAt); // Commented out due to changes in Video 2
+		// Raycast Stuff.
+		// Bit shift the index of the layer (9) to get a bit mask
+		// This will cast rays only against colliders in layer 9.
+		int layerMask = 1 << 9;
+		RaycastHit hit;
+		// If the ray intersects any objects in layer 9...
+		if (Physics.Raycast(castFrom.transform.position, castTo.transform.position, out hit, idealCamOffset, layerMask))
+		{
+			float clippedCamOffset = hit.distance + camOffsetOffset; // clippedCamOffset is set to the distance between the rays orign point and the rays hit point plus the offsets offset. 
+			camOffset = clippedCamOffset;	// The cameras offset is set to the clippedCamOffset.
+			//Debug.Log("Did Hit");
+			//Debug.Log(hit.distance);
+		}
+		//	If the ray dosn't intersect any valid objects...
+		else
+		{
+			camOffset = idealCamOffset;	// The cameras offset is set to the ideal offset.
+			//Debug.Log("Did not Hit");
+		}
+
+		Vector3 dir = new Vector3(0, 0, -camOffset);						//
+		Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);		//
+		camTransform.position = thePlayer.position + rotation * dir;		//	
+		camTransform.LookAt(thePlayer.position);							//	Make the camera look at the player.
 	}
 
 }
